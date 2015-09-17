77252088001815ba18d59fae5ad0ebe90aefa83f
Faustino Tavares de Santana Filho <faustino.tsf@gmail.com>
2015-09-17 00:33:23 -0300

    Organização de pastas
    	* Foi colocado todos os arquivos na pasta public_html

---
+++ public_html/js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.titulo = 'Faustino Tavares'; 
+  $scope.promo = 'Teste H2';
+  $scope.produto = { 
+      nome: 'Xfrango', 
+      preco: 19 
+    }
+}]);
